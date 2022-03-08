9cc3a878062e3239c4e7721d2477fb15acecdc36
Thom Chiovoloni <chiovolonit@gmail.com>
2022-03-08 10:56:02 -0800

    use `minicbor::bytes` (and `serde_bytes`, althoguh it should not matter for BARE except for performance)

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
-use minicbor::{Decode, Encode};
+use minicbor::{bytes::ByteVec, Decode, Encode};
 use rand::distributions::{Alphanumeric, Standard};
 use rand::{rngs::ThreadRng, Rng, RngCore};
 use serde::{Deserialize, Serialize};
 use std::iter;
 
-#[derive(Debug, Clone, Encode, Decode, Serialize, Deserialize)]
-struct TransportMessage {
+#[derive(Debug, Clone, Encode, Decode)]
+struct TransportMessageCbor {
     #[n(0)]
     version: u8,
     #[n(1)]
-    onward_route: Vec<Vec<u8>>,
+    onward_route: Vec<minicbor::bytes::ByteVec>,
     #[n(2)]
-    return_route: Vec<Vec<u8>>,
+    return_route: Vec<minicbor::bytes::ByteVec>,
     #[n(3)]
+    #[cbor(with = "minicbor::bytes")]
+    payload: Vec<u8>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct TransportMessageSerde {
+    version: u8,
+    onward_route: Vec<serde_bytes::ByteBuf>,
+    return_route: Vec<serde_bytes::ByteBuf>,
+    #[serde(with = "serde_bytes")]
     payload: Vec<u8>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let inner_bare = serde_bare::to_vec(&TransportMessage {
+    let inner_bare = serde_bare::to_vec(&TransportMessageSerde {
         version: 0,
-        onward_route: vec!["0#app".as_bytes().to_vec()],
+        // `serde_bytes::ByteBuf` doesn't implement `From`, and just has a
+        // constructor function named `from` :/
+        onward_route: vec![serde_bytes::ByteBuf::from("0#app".as_bytes())],
         return_route: vec![],
         payload: serde_bare::to_vec(&String::from("Hello ockam"))?,
     })?;
 
-    let inner_cbor = minicbor::to_vec(&TransportMessage {
+    let inner_cbor = minicbor::to_vec(&TransportMessageCbor {
         version: 0,
-        onward_route: vec!["0#app".as_bytes().to_vec()],
+        onward_route: vec!["0#app".as_bytes().to_vec().into()],
         return_route: vec![],
         payload: minicbor::to_vec(&String::from("Hello ockam"))?,
     })?;
@@ -34,21 +46,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let len_inner_bare = inner_bare.len();
     let len_inner_cbor = inner_cbor.len();
 
-    let outer_bare = serde_bare::to_vec(&TransportMessage {
+    let outer_bare = serde_bare::to_vec(&TransportMessageSerde {
         version: 0,
         onward_route: vec![
-            "1#hub.ockam.network".as_bytes().to_vec(),
-            "0#my-pipe-friend".as_bytes().to_vec(),
+            serde_bytes::ByteBuf::from("1#hub.ockam.network".as_bytes().to_vec()),
+            serde_bytes::ByteBuf::from("0#my-pipe-friend".as_bytes().to_vec()),
         ],
         return_route: vec![],
         payload: inner_bare,
     })?;
 
-    let outer_cbor = minicbor::to_vec(&TransportMessage {
+    let outer_cbor = minicbor::to_vec(&TransportMessageCbor {
         version: 0,
         onward_route: vec![
-            "1#hub.ockam.network".as_bytes().to_vec(),
-            "0#my-pipe-friend".as_bytes().to_vec(),
+            "1#hub.ockam.network".as_bytes().to_vec().into(),
+            "0#my-pipe-friend".as_bytes().to_vec().into(),
         ],
         return_route: vec![],
         payload: inner_cbor,
@@ -57,21 +69,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let len_outer_bare = outer_bare.len();
     let len_outer_cbor = outer_cbor.len();
 
-    let outest_bare = serde_bare::to_vec(&TransportMessage {
+    let outest_bare = serde_bare::to_vec(&TransportMessageSerde {
         version: 0,
         onward_route: vec![
-            "1#hub.ockam.network".as_bytes().to_vec(),
-            "0#my-pipe-friend".as_bytes().to_vec(),
+            serde_bytes::ByteBuf::from("1#hub.ockam.network".as_bytes().to_vec()),
+            serde_bytes::ByteBuf::from("0#my-pipe-friend".as_bytes().to_vec()),
         ],
         return_route: vec![],
         payload: outer_bare,
     })?;
 
-    let outest_cbor = minicbor::to_vec(&TransportMessage {
+    let outest_cbor = minicbor::to_vec(&TransportMessageCbor {
         version: 0,
         onward_route: vec![
-            "1#hub.ockam.network".as_bytes().to_vec(),
-            "0#my-pipe-friend".as_bytes().to_vec(),
+            "1#hub.ockam.network".as_bytes().to_vec().into(),
+            "0#my-pipe-friend".as_bytes().to_vec().into(),
         ],
         return_route: vec![],
         payload: outer_cbor,
